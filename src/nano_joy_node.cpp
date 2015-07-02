75e29269cf86f4cd104ace43f3c7f345d3298db2
Weikun Zhen <zhenwk@gmail.com>
2015-07-02 17:00:22 -0400

    added controller switch between PID and joystick

--- src/nano_joy_node.cpp
+++ src/nano_joy_node.cpp
@@ -15,58 +15,123 @@ public:
         cmd_msg.stamp  = ros::Time::now();
         cmd_pub.publish(cmd_msg);
     }
+    // send command 'selectively'
+    void sendCommand();
+
+    // check channel switch button
+    void start();
 
 private:
     // joy stick callback function
     void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 
-    double max_roll, max_pitch, max_yaw, max_thrust;
+    // cf_controller callback function
+    void cfCallback(const nano_quad_ros::crazyflieCommand::ConstPtr& c);
+
+    double MAX_ROLL, MAX_PITCH, MAX_YAW, MAX_THRUST;
+    bool CTL_SWITCH;
+    std::string ctl_channel;
 
     ros::Publisher cmd_pub;
-    ros::Subscriber joy_sub;
+    ros::Subscriber js_sub;
+    ros::Subscriber cf_sub;
+
+    // command values
+    nano_quad_ros::crazyflieCommand cf;
+    nano_quad_ros::crazyflieCommand js;
 };
 
 NanoJoyNode::NanoJoyNode(ros::NodeHandle nh)
 {
     // get parameter from launch file
-    nh.getParam("max_roll", max_roll);
-    nh.getParam("max_pitch", max_pitch);
-    nh.getParam("max_yaw", max_yaw);
-    nh.getParam("max_thrust", max_thrust);
+    nh.getParam("max_roll",    MAX_ROLL);
+    nh.getParam("max_pitch",   MAX_PITCH);
+    nh.getParam("max_yaw",     MAX_YAW);
+    nh.getParam("max_thrust",  MAX_THRUST);
+    nh.getParam("ctl_channel", ctl_channel);
+
+    CTL_SWITCH = true;
 
     // publish crazyflie 2.0 command
-    cmd_pub = nh.advertise<nano_quad_ros::crazyflieCommand>("/nano_quad_ros/crazyflieCommand", 1);
+    cmd_pub = nh.advertise<nano_quad_ros::crazyflieCommand>("/nano_joy/crazyflieCommand", 1);
 
     // subscribe to joystick topic
-    joy_sub = nh.subscribe<sensor_msgs::Joy>("/joy", 10, &NanoJoyNode::joyCallback, this);
+    js_sub = nh.subscribe<sensor_msgs::Joy>("/joy", 10, &NanoJoyNode::joyCallback, this);
+
+    // subscribe to cf_controller topic
+    cf_sub = nh.subscribe<nano_quad_ros::crazyflieCommand>("/cf_controller/crazyflieCommand",
+                                                           10, &NanoJoyNode::cfCallback, this);
 }
 
 void NanoJoyNode::joyCallback(const sensor_msgs::Joy::ConstPtr &joy)
 {
-    nano_quad_ros::crazyflieCommand cmd_msg;
-    double roll, pitch, yaw, thrust;
+    double roll, pitch, yaw, thrust, sw;
 
-    roll   = -max_roll   * joy->axes[2];
-    pitch  = -max_pitch  * joy->axes[3];
-    yaw    =  max_yaw    * joy->axes[0];
-    thrust =  max_thrust * joy->axes[1];
+    roll   = -MAX_ROLL   * joy->axes[2];
+    pitch  = -MAX_PITCH  * joy->axes[3];
+    yaw    =  MAX_YAW    * joy->axes[0];
+    thrust =  MAX_THRUST * joy->axes[1];
+    sw     =               joy->axes[13];
 
-    cmd_msg.stamp  = ros::Time::now();
-    cmd_msg.roll   = roll;
-    cmd_msg.pitch  = pitch;
-    cmd_msg.yaw    = yaw;
-    cmd_msg.thrust = (int)thrust;
+    js.stamp  = ros::Time::now();
+    js.roll   = roll;
+    js.pitch  = pitch;
+    js.yaw    = yaw;
+    js.thrust = (int)thrust;
+    if(js.thrust < 0) js.thrust = 0;
 
-    if(cmd_msg.thrust < 0) cmd_msg.thrust = 0;
-
-    cmd_pub.publish(cmd_msg);
+    // check if controller should be started
+    if(sw < 1.0)
+        CTL_SWITCH = true;
+    else
+        CTL_SWITCH = false;
 }
 
+void NanoJoyNode::cfCallback(const nano_quad_ros::crazyflieCommand::ConstPtr &c)
+{
+    cf = *c;
+}
+
+void NanoJoyNode::sendCommand()
+{
+    nano_quad_ros::crazyflieCommand cmd(js);
+
+    if(CTL_SWITCH)
+    {
+        if(ctl_channel == "roll"  ) cmd.roll   = cf.roll;
+        if(ctl_channel == "pitch" ) cmd.pitch  = cf.pitch;
+        if(ctl_channel == "yaw"   ) cmd.yaw    = cf.yaw;
+        if(ctl_channel == "thrust") cmd.thrust = cf.thrust;
+        ROS_INFO("PID dominates: %s",ctl_channel.c_str());
+    }
+    else ROS_INFO("PID dominates: (none)");
+
+    cmd_pub.publish(cmd);
+}
+
+void NanoJoyNode::start()
+{
+    ROS_INFO("Press switcher to start:");
+    while(CTL_SWITCH)
+    {
+        ros::spinOnce();
+        usleep(10000);
+    }
+}
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "nano_joy_node");
     ros::NodeHandle n("~");
     NanoJoyNode nj(n);
 
-    ros::spin();
+    nj.start();
+
+    ros::Rate r(30);
+    while(n.ok()){
+        nj.sendCommand();
+
+        ros::spinOnce();
+        r.sleep();
+    }
+    return 0;
 }
